a25361b1de3cc82aea58c36a3cf53cae8d90aec9
avaneesh2210 <72684066+avaneesh2210@users.noreply.github.com>
2021-06-24 16:49:21 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBmhrlX6W1cH8jFkEwx8IxyqKSFPGB9LuE",
+    authDomain: "lets-chat-f0699.firebaseapp.com",
+    databaseURL: "https://lets-chat-f0699-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-f0699",
+    storageBucket: "lets-chat-f0699.appspot.com",
+    messagingSenderId: "110087400479",
+    appId: "1:110087400479:web:16a21ad927872b6d42f92a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
